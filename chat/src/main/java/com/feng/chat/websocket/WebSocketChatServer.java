7590fece8c12f29522ad3ec75f947762d5a609e9
jack_txf <762600503@qq.com>
2024-08-26 20:38:46 +0800

    chat-原始的方式

--- chat/src/main/java/com/feng/chat/websocket/WebSocketChatServer.java
+++ chat/src/main/java/com/feng/chat/websocket/WebSocketChatServer.java
@@ -3,13 +3,17 @@ package com.feng.chat.websocket;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
+// 比较原始的方法
 @Component
-@ServerEndpoint("/txfChat")
+@ServerEndpoint("/txfChat/{token}")
 @Slf4j
 public class WebSocketChatServer {
     /**
@@ -21,9 +25,13 @@ public class WebSocketChatServer {
      * 当客户端打开连接：1.添加会话对象 2.更新在线人数
      */
     @OnOpen
-    public void onOpen(Session session) {
-        onlineSessions.put(session.getId(), session);
-        // 向所有人发一个消息
+    public void onOpen(Session session, @PathParam("token")String token) {
+        if ( !onlineSessions.containsKey(token) ) {
+            onlineSessions.put(token, session);
+            // 向所有人发一个消息
+            sendMessageToAll("【系统消息】:" + token + "上线了!!~");
+            System.out.println("当前在线人数：" + onlineSessions.size());
+        }
     }
 
     /**
@@ -31,7 +39,25 @@ public class WebSocketChatServer {
      */
     @OnMessage // 服务器接收到消息的时候
     public void onMessage(Session session, String jsonStr) {
+        System.out.println(session);
+        System.out.println("收到消息：" + jsonStr);
+        sendMessageToAllExcludeMe(session, jsonStr);
+    }
 
+    private void sendMessageToAllExcludeMe(Session session, String jsonStr) {
+        String userToken = onlineSessions.entrySet()
+                .stream().filter(item -> item.getValue() == session)
+                .map(Map.Entry::getKey).collect(Collectors.joining());
+        String msg = "{ "+ userToken +" }：" + jsonStr;
+        onlineSessions.forEach((id, session1) -> {
+            try {
+                if ( session != session1) { // 如果不是自己
+                    session1.getBasicRemote().sendText(msg);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     /**
@@ -39,8 +65,10 @@ public class WebSocketChatServer {
      */
     @OnClose
     public void onClose(Session session) {
-        onlineSessions.remove(session.getId());
-
+        Collection<Session> values = onlineSessions.values();
+        while( values.contains(session))
+            values.remove(session);
+        System.out.println("当前人数：" + onlineSessions.size());
     }
 
     /**
