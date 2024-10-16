8bad28ca27a8f5e93158b6861a4d0991ef0efa2b
jack_txf <762600503@qq.com>
2024-10-16 14:05:09 +0800

    测试

--- yt-tool/src/test/java/com/test.java
+++ yt-tool/src/test/java/com/test.java
@@ -2,24 +2,40 @@ package com;
 
 import org.junit.platform.engine.discovery.PackageSelector;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Random;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author Williams_Tian
  * @CreateDate 2024/9/2
  */
 public class test {
-    public static void main(String[] args) {
-        try {
-            test1();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\Users\\xinhua\\Desktop\\book.txt";
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+
+        List<String> bookIds = new ArrayList<>();
+        String bookId = "";
+        while ( (bookId = reader.readLine()) != null ) {
+            bookIds.add(bookId);
         }
+
+        // 把这些书 序列化后放入文本文件中
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < bookIds.size(); i++) {
+            if ( i != 0 ) s.append(",").append("'").append(bookIds.get(i)).append("'");
+            else s.append("'").append(bookIds.get(i)).append("'");
+        }
+        System.out.println(s);
+
+        reader.close();
+        //try {
+        //    test1();
+        //} catch (IOException e) {
+        //    throw new RuntimeException(e);
+        //}
     }
 
     public static void test1() throws IOException, IndexOutOfBoundsException {
