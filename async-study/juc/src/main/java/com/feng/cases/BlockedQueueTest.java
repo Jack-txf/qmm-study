50fe2d547d7ce87efb019fbb0595482d03b84a36
jack_txf <762600503@qq.com>
2025-04-28 17:15:22 +0800

    juc-阻塞队列

--- async-study/juc/src/main/java/com/feng/cases/BlockedQueueTest.java
+++ async-study/juc/src/main/java/com/feng/cases/BlockedQueueTest.java
@@ -14,3 +14,16 @@ public class BlockedQueueTest {
         BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(10);
     }
 }
+
+class TBlockedQueue<T> {
+    public int capacity;
+    public int size;
+
+    public TBlockedQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    // 1. 入队 -- 当队列已满时，向队列中添加元素的操作会被阻塞，直到队列有空间可用。
+    //public void put();
+    // 2. 出队 -- 当队列为空时，从队列中获取元素的操作会被阻塞，直到队列中有新元素加入
+}
