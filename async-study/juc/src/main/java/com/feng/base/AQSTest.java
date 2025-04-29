dd367271dca5462b204b0d4604fdf4d7dc87b327
jack_txf <762600503@qq.com>
2025-04-29 15:45:15 +0800

    aqs

---
+++ async-study/juc/src/main/java/com/feng/base/AQSTest.java
@@ -0,0 +1,17 @@
+package com.feng.base;
+
+import java.util.concurrent.locks.AbstractQueuedLongSynchronizer;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @version 1.0
+ * @Author
+ * @Date 2025/4/29 9:52
+ * @注释
+ */
+public class AQSTest {
+    public static void main(String[] args) {
+        //AbstractQueuedSynchronizer a = new AbstractQueuedSynchronizer();
+        //AbstractQueuedLongSynchronizer b = new AbstractQueuedLongSynchronizer();
+    }
+}
