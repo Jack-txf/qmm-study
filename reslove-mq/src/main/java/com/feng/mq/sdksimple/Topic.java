fc5f35d8327813b29a8cf3228094a29841be26b5
jack_txf <762600503@qq.com>
2025-05-21 17:05:46 +0800

    rocketmq -- 惨

---
+++ reslove-mq/src/main/java/com/feng/mq/sdksimple/Topic.java
@@ -0,0 +1,11 @@
+package com.feng.mq.sdksimple;
+
+/**
+ * @version 1.0
+ * @Author
+ * @Date 2025/5/21 15:12
+ * @注释
+ */
+public class Topic {
+    public static String DEMO_TOPIC = "demo-topic";
+}
--- reslove-mq/src/main/java/com/feng/mq/sdksimple/producer/SimpleMsgProducer.java
+++ reslove-mq/src/main/java/com/feng/mq/sdksimple/producer/SimpleMsgProducer.java
@@ -1,12 +1,17 @@
 package com.feng.mq.sdksimple.producer;
 
+import com.feng.mq.sdksimple.Topic;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientConfigurationBuilder;
 import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
 import org.apache.rocketmq.client.apis.message.Message;
 import org.apache.rocketmq.client.apis.message.MessageBuilder;
 import org.apache.rocketmq.client.apis.producer.Producer;
 import org.apache.rocketmq.client.apis.producer.ProducerBuilder;
 import org.apache.rocketmq.client.apis.producer.SendReceipt;
+import org.apache.rocketmq.client.java.example.ProducerSingleton;
 import org.apache.rocketmq.client.java.impl.producer.ProducerBuilderImpl;
 import org.apache.rocketmq.client.java.message.MessageBuilderImpl;
 
@@ -20,30 +25,45 @@ import java.io.IOException;
 @Slf4j
 public class SimpleMsgProducer {
     public static void main(String[] args) {
-
+        try {
+            sendSyncMessage();
+        } catch (ClientException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     // 普通消息发送
-    public void sendSyncMessage() throws ClientException, IOException {
-        ProducerBuilder p = new ProducerBuilderImpl();
-        Producer producer = p.build();
+    public static void sendSyncMessage() throws ClientException, IOException {
+        // 接入点地址，需要设置成Proxy的地址和端口列表，一般是xxx:8080;xxx:8081。
+        String endpoint = "192.168.110.128:8080";
+        // 消息发送的目标Topic名称，需要提前创建。
+        String topic = Topic.DEMO_TOPIC;
 
-        MessageBuilder messageBuilder = new MessageBuilderImpl();
-        Message message = messageBuilder.setTopic("topic")
-                //设置消息索引键，可根据关键字精确查找某条消息。
+        ClientServiceProvider provider = ClientServiceProvider.loadService();
+        ClientConfigurationBuilder builder = ClientConfiguration.newBuilder().setEndpoints(endpoint)
+                .enableSsl(false);
+        ClientConfiguration configuration = builder.build();
+        // 初始化Producer时需要设置通信配置以及预绑定的Topic。
+        Producer producer = provider.newProducerBuilder()
+                .setTopics(topic)
+                .setClientConfiguration(configuration)
+                .build();
+        // 普通消息发送。
+        Message message = provider.newMessageBuilder()
+                .setTopic(topic)
+                // 设置消息索引键，可根据关键字精确查找某条消息。
                 .setKeys("messageKey")
-                //设置消息Tag，用于消费端根据指定Tag过滤消息。
+                // 设置消息Tag，用于消费端根据指定Tag过滤消息。
                 .setTag("messageTag")
-                //消息体。
+                // 消息体。
                 .setBody("messageBody".getBytes())
                 .build();
         try {
-            //发送消息，需要关注发送结果，并捕获失败等异常。
+            // 发送消息，需要关注发送结果，并捕获失败等异常。
             SendReceipt sendReceipt = producer.send(message);
-            System.out.println(sendReceipt.getMessageId());
+            System.out.println("Send message successfully, messageId={}: "+  sendReceipt.getMessageId());
         } catch (ClientException e) {
-            e.printStackTrace();
+            System.out.println("Failed to send message" + e);;
         }
-
-        producer.close();
+         producer.close();
     }
 }
